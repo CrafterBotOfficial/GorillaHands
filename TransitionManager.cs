8bb41a7646ad6f5f0683de9449accc62a9e01453
Crafterbot <crafterbot@proton.me>
2025-08-10 13:25:40 -0400

    Improved hand opening

--- TransitionManager.cs
+++ TransitionManager.cs
@@ -10,12 +10,20 @@ public class TransitionManager
 
     private HandState handState;
     private float transitionT;
+    private Vector3 targetPosition;
+
+#if DEBUG
+    private LineRenderer lineRenderer_PlayerHandToTargetPos_Debug;
+#endif
 
     public TransitionManager(HandController controller, XRNode inputDevice)
     {
         Controller = controller;
         InputDevice = inputDevice;
         handState = HandState.Closed;
+#if DEBUG
+        lineRenderer_PlayerHandToTargetPos_Debug = Controller.CreateDebugLine(.1f);
+#endif
     }
 
     public bool HandHidden() => handState == HandState.Closed;
@@ -31,12 +39,16 @@ public class TransitionManager
             return true;
         }
 
-        // Todo: make not ugly - temp code
-        if (/*!inTransition &&*/ buttonPressed)
+        if (buttonPressed)
         {
             handState = handState == HandState.Open ? HandState.Closing : HandState.Opening;
             Main.Log($"Setting hand state to {handState}");
-            if (handState == HandState.Opening) transitionT = 1; // I'd prefer just use an animator as this feels tacky
+            if (handState == HandState.Opening)
+            {
+                transitionT = 1; // I'd prefer just use an animator as this feels tacky
+                targetPosition = GetHandSpawnPoint();
+            }
+            else targetPosition = Controller.TargetPosition;
             HandleHandTransition();
             return true;
         }
@@ -55,26 +67,47 @@ public class TransitionManager
 
         // todo: hands should phase in (close) from there actual position, not the target pos. (when the hands get stuck it looks ugly)
         // hand movement
-        Vector3 phaseTarget = Vector3.Lerp(Controller.TargetPosition, Controller.PlayerHand.position,  transitionT);
+        Vector3 phaseTarget = Vector3.Lerp(targetPosition, Controller.PlayerHand.position, transitionT);
         Controller.Follower.position = phaseTarget;
         Controller.FollowerRigidbody.velocity = Vector3.zero;
 
         // hand rot
-        Controller.ApplyRotationaryForce(); 
+        Controller.ApplyRotationaryForce();
 
-        if (AnimationComplete()) {
+        if (AnimationComplete())
+        {
             Main.Log("Completed animation, or sorry transition");
             handState = handState == HandState.Opening ? HandState.Open : HandState.Closed;
         }
     }
 
-    private bool AnimationComplete() {
-        if (handState == HandState.Opening) 
+    private bool AnimationComplete()
+    {
+        if (handState == HandState.Opening)
             return transitionT <= 0;
         if (handState == HandState.Closing)
             return transitionT >= 1;
         Main.Log("What is even going on anymore?");
         return false;
+    }
+
+    // Todo: When hands spawn to close to player (like if the player ius pointing straight down) the player will be flung as the hands will clip into him
+    private Vector3 GetHandSpawnPoint()
+    {
+        Vector3 direction = Controller.TargetPosition - Controller.PlayerHand.position;
+        float distance = Vector3.Distance(Controller.PlayerHand.position, Controller.TargetPosition);
+        Ray ray = new Ray(Controller.PlayerHand.position, direction);
+
+        if (!Physics.Raycast(Controller.PlayerHand.position, direction, out RaycastHit hit, distance, Controller.TerrainLayers))
+            return Controller.TargetPosition;
+
+#if DEBUG
+        lineRenderer_PlayerHandToTargetPos_Debug.SetPositions([Controller.PlayerHand.position, hit.point]);
+#endif
+        // moves hand closer to avoid clipping into terrain
+        Vector3 unit = direction.normalized;
+        Vector3 spawn = hit.point - unit;
+        return spawn;
     }
 
     private enum HandState
